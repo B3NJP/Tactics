616e19274789bdabf479e8315b1882c0b2da1ff2
B3NJP <b3np@pearcy.org>
2019-02-13 10:53:56 +1100

    Add clicking

--- Map Creator/Map Creator.py
+++ Map Creator/Map Creator.py
@@ -30,6 +30,9 @@ while True:
                 area[1] += .2
             if event.key == pygame.K_LEFT:
                 area[0] -= .2
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            grid[int((event.pos[1]-area[1]*100)//100)][int((event.pos[0]-area[0]*100)//100)] = 1
+            # print((event.pos[0]-area[0]*100)//100)
 
     screen.fill(white)
     # screen.fill((black), box)
@@ -37,5 +40,8 @@ while True:
         for j in range(0, len(grid[i])):
             if (i+area[1] < 700 and i+area[1] > -100):
                 if (j+area[0] < 700 and j+area[0] > -100):
-                    pygame.draw.rect(screen, black, [(j+area[0])*100, (i+area[1])*100]+box, 1)
+                    if grid[i][j] == 0:
+                        pygame.draw.rect(screen, black, [(j+area[0])*100, (i+area[1])*100]+box, 1)
+                    # else:
+                        # pygame.draw.rect(screen, (50, 0, 0), [(j+area[0])*100, (i+area[1])*100]+box, 1)
     pygame.display.flip()
