4b416a862372ce1b529a198913d3c931bc04e6e0
B3NJP <b3np@pearcy.org>
2019-02-13 10:38:18 +1100

    Added basic view

---
+++ Map Creator/Map Creator.py
@@ -0,0 +1,41 @@
+import sys, pygame
+pygame.init()
+box = [100, 100]
+size = [700, 700]
+black = 0, 0, 0
+white = 255, 255, 255
+
+area = [0, 0]
+
+grid = [
+[0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0]
+]
+
+screen = pygame.display.set_mode(size)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                area[1] -= .2
+            if event.key == pygame.K_RIGHT:
+                area[0] += .2
+            if event.key == pygame.K_DOWN:
+                area[1] += .2
+            if event.key == pygame.K_LEFT:
+                area[0] -= .2
+
+    screen.fill(white)
+    # screen.fill((black), box)
+    for i in range(0, len(grid)):
+        for j in range(0, len(grid[i])):
+            if (i+area[1] < 700 and i+area[1] > -100):
+                if (j+area[0] < 700 and j+area[0] > -100):
+                    pygame.draw.rect(screen, black, [(j+area[0])*100, (i+area[1])*100]+box, 1)
+    pygame.display.flip()
