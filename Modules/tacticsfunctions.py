30982b5021e6eedb896cf7b4250e3381bbfaa7a2
B3NJP <b3np@pearcy.org>
2019-02-14 16:51:04 +1100

    Added moveAble
    moveAble finds area unit can move to
    Also fixed a lot of issues

--- Modules/tacticsfunctions.py
+++ Modules/tacticsfunctions.py
@@ -9,5 +9,35 @@ class Actions(Enum):
     MOVE = auto()
     SELECT = auto()
 
+def moveAble(person, grid, tiles):
+    queue = [[person.location, person.mov]]
+    moveTo = []
+    while queue:
+        current = queue.pop(0)
+        moveTo += [current[0]]
+        potential = []
+
+        potential += [
+        [current[0][0]+1, current[0][1]],
+        [current[0][0]-1, current[0][1]],
+        [current[0][0], current[0][1]+1],
+        [current[0][0], current[0][1]-1]
+        ]
+
+        for i in potential:
+            if not i in moveTo:
+                if (i[1] >= 0 and i[1] < len(grid)) and (i[0] >= 0 and i[0] < len(grid[i[1]])):
+                    tempTile = None
+                    for j in tiles:
+                        if grid[i[1]][i[0]] == j.key:
+                            tempTile = j
+                            break
+                    if current[1] - tempTile.mov >= 0:
+                        queue += [[i, current[1] - tempTile.mov]]
+
+    return moveTo
+
+
+
 def end():
     sys.exit()
--- Tactics.py
+++ Tactics.py
@@ -11,7 +11,7 @@ area = [0, 0]
 grid = []
 
 for line in fileinput.input():
-    grid += [line.split(",")]
+    grid += [line.rstrip("\n").split(",")]
 
 screen = pygame.display.set_mode(size)
 
@@ -24,13 +24,15 @@ p1 = pygame.transform.scale(pygame.image.load("Assets/Tiles/P1.png"), box)
 selected = "0"
 
 # name, job, race, stats, growthRates, location = [0,0], abilities = [], weapon = None, items = []
-unit1 = TacticsClasses.Person("1", TacticsPresets.exampleJob, TacticsPresets.exampleRace, [10, 10, 10, 10, 10, 10, 10, 10, 10], [10, 10, 10, 10, 10, 10, 10, 10, 10])
+unit1 = TacticsClasses.Person("1", TacticsPresets.exampleJob, TacticsPresets.exampleRace, [10, 10, 10, 10, 10, 10, 10, 10, 5], [10, 10, 10, 10, 10, 10, 10, 10])
 
 units = []
 units += [unit1]
 
 action = tacticsfunctions.Actions.SELECT
 selected = None
+
+moveTo = []
 
 while True:
     for event in pygame.event.get():
@@ -49,13 +51,14 @@ while True:
                 selected = "h"
             if event.key == pygame.K_0:
                 selected = "0"
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            location = [(event.pos[0]-area[0]*100)//100, (event.pos[1]-area[1]*100)//100]
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            location = [int((event.pos[0]-area[0]*100)//100), int((event.pos[1]-area[1]*100)//100)]
             if action == tacticsfunctions.Actions.SELECT:
                 for i in units:
                     if (i.location[0] == location[0]) and (i.location[1] == location[1]):
                         selected = i
                         action = tacticsfunctions.Actions.MOVE
+                        moveTo = tacticsfunctions.moveAble(selected, grid, tiles)
                         break
             elif action == tacticsfunctions.Actions.MOVE:
                 tacticsfunctions.move(selected, location)
@@ -63,10 +66,18 @@ while True:
 
     screen.fill(white)
     # screen.fill((black), box)
+    if action == tacticsfunctions.Actions.MOVE:
+        for i in range(0, len(grid)):
+            for j in range(0, len(grid[i])):
+                if ((i+area[1])*100 < 700 and (i+area[1])*100 > -100):
+                    if ((j+area[0])*100 < 700 and (j+area[0])*100 > -100):
+                        if [j, i] in moveTo:
+                            screen.fill((0, 0, 255), [max((j+area[0])*100, 0), max((i+area[1])*100, 0)] + [min((area[0] + j + 1)*100, 100), min((area[1] + i + 1)*100, 100)])
+
     for i in range(0, len(grid)):
         for j in range(0, len(grid[i])):
-            if (i+area[1] < 700 and i+area[1] > -100):
-                if (j+area[0] < 700 and j+area[0] > -100):
+            if ((i+area[1])*100 < 700 and (i+area[1])*100 > -100):
+                if ((j+area[0])*100 < 700 and (j+area[0])*100 > -100):
                     pygame.draw.rect(screen, black, [(j+area[0])*100, (i+area[1])*100]+box, 1)
                     for k in tiles:
                         if grid[i][j] == k.key:
@@ -75,4 +86,5 @@ while True:
 
     for i in units:
         screen.blit(p1, [(i.location[0]+area[0])*100, (i.location[1]+area[1])*100])
+
     pygame.display.flip()
