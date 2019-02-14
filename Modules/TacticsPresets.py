0a9068a1ec7ee705b532ad35f32a8249b753df36
B3NJP <b3np@pearcy.org>
2019-02-14 17:27:02 +1100

    Removed things

--- Modules/TacticsPresets.py
+++ Modules/TacticsPresets.py
@@ -8,6 +8,8 @@ exampleAbility = TacticsClasses.Ability(name = "example", type = "magic", range 
 # REMEMBER:
 # health, mana, pAtk, mAtk, dfce, res, agi, skl, abilities
 
+
+
 # Growth Rate Priority:
 # Race > Job > Base
 
@@ -15,21 +17,6 @@ exampleAbility = TacticsClasses.Ability(name = "example", type = "magic", range 
 # Spells
 fireball = TacticsClasses.Ability(name = "fireball", type = "magic", range = 20, dmg = 3, dmgType = "mAtk", mpCost = 1)
 lightning = TacticsClasses.Ability(name = "lightning", type = "magic", range = 30, dmg = 5, dmgType = "mAtk", mpCost = 3)
-
-death = TacticsClasses.Ability(name = "death", type = "magic", range = 40, dmg = 100, dmgType = "mAtk", mpCost = 100)
-
-def tele(values, targets):
-    values[0] = values[1]
-teleport = TacticsClasses.Ability(name = "teleport", type = "magic", range = 40, dmgType = "special", mpCost = 3, special = tele)
-
-
-# Racial
-firebreath = TacticsClasses.Ability(name = "firebreath", type = "racial", range = 5, dmg = 7, dmgType = "mAtk", cooldown = 10)
-
-def eatsoul (values, target):
-    if (target.health <= 0):
-        values[0].health = min(values[0].health + values[0].maxHealth*0.1, values[0].maxHealth)
-eatSoul = TacticsClasses.Ability(name = "eatSoul", type = "racial", range = 1, multi = 2, dmgType = "unresistable", cooldown = 10, special = eatsoul)
 
 # Races
 # human = TacticsClasses.Race("human", 0.6, 0.1, 0.6, 0.2, 0.5, 0.4, 0.4, 0.4, [])
