a8c59785a5097aab9d832b7fb3db124e7d69bdd6
B3NJP <b3np@pearcy.org>
2019-02-15 13:31:04 +1100

    Fixed things

--- Modules/xmlProcessing.py
+++ Modules/xmlProcessing.py
@@ -5,20 +5,20 @@ def toDictionary(element):
     tDict = {}
     for i in element:
         if i.type == "text":
-            tDict[i.name] = i.text
+            tDict[i.get("name")] = i.text
         if i.type == "int":
-            tDict[i.name] = int(i.text)
+            tDict[i.get("name")] = int(i.text)
         if i.type == "float":
-            tDict[i.name] = float(i.text)
+            tDict[i.get("name")] = float(i.text)
         if i.type == "array":
             if i.type2 == "text":
-                tDict[i.name] = [j.text for j in i]
+                tDict[i.get("name")] = [j.text for j in i]
             if i.type2 == "int":
-                tDict[i.name] = [int(j.text) for j in i]
+                tDict[i.get("name")] = [int(j.text) for j in i]
             if i.type2 == "float":
-                tDict[i.name] = [float(j.text) for j in i]
+                tDict[i.get("name")] = [float(j.text) for j in i]
             if i.type2 == "object":
-                tDict[i.name] = [All[j.type][j.name] for j in i]
+                tDict[i.get("name")] = [All[j.type][j.name] for j in i]
 
     return tDict
 
@@ -36,4 +36,4 @@ tree = ET.parse()
 root = tree.getroot()
 for i in root:
     for j in i:
-        All[j.tag]
+        All[j.tag][j.get(name)] = toDictionary(j)
