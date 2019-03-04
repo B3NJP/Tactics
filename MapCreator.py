e577bd0a3376bf5d869b1d6905c0ee13a05e0842
B3NJP <b3np@pearcy.org>
2019-03-04 13:48:49 +1100

    Slowly fixing MapCreator

--- MapCreator.py
+++ MapCreator.py
@@ -15,34 +15,34 @@ for line in fileinput.input():
 
 screen = pygame.display.set_mode(size)
 
-# hill = pygame.transform.scale(pygame.image.load("../Assets/Tiles/hill.png"), box)
-tiles = [
-TacticsPresets.plain,
-TacticsPresets.hill
-]
+all = xmlProcessing.All
 
-selected = "0"
+# hill = pygame.transform.scale(pygame.image.load("../Assets/Tiles/hill.png"), box)
+tiles = list(all["tile"].values())
+
+selected = tiles[0]
+
+spot = 25 # Spot from bottom to display usableAbilities
+menu = pygame.Surface((300, 400)) # Creates menu surface
+page = 0 # Creates menu page
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             mapcreatorfunctions.end(grid)
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-                area[1] += .2
-            if event.key == pygame.K_RIGHT:
-                area[0] -= .2
-            if event.key == pygame.K_DOWN:
-                area[1] -= .2
-            if event.key == pygame.K_LEFT:
-                area[0] += .2
-            if event.key == pygame.K_h:
-                selected = "h"
-            if event.key == pygame.K_0:
-                selected = "0"
+            if pygame.key.get_mods() & pygame.KMOD_SHIFT:
+                if event.key == pygame.K_UP:
+                    area[1] += .2
+                if event.key == pygame.K_RIGHT:
+                    area[0] -= .2
+                if event.key == pygame.K_DOWN:
+                    area[1] -= .2
+                if event.key == pygame.K_LEFT:
+                    area[0] += .2
         if event.type == pygame.MOUSEBUTTONDOWN:
             location = [(event.pos[0]-area[0]*100)//100, (event.pos[1]-area[1]*100)//100]
-            grid[int(location[1])][int(location[0])] = selected
+            grid[int(location[1])][int(location[0])] = selected.key
             # print((event.pos[0]-area[0]*100)//100)
 
     screen.fill(white)
@@ -56,5 +56,8 @@ while True:
                         if grid[i][j] == k.key:
                             screen.blit(k.img, [(j+area[0])*100, (i+area[1])*100])
                             break
+
+    # Print menu
+    screen.blit(menu, [0, 700-300])
 
     pygame.display.flip()
