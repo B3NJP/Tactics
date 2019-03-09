fb0394320e57e5af08ae019f374535e94e9ab975
B3NJP <b3np@pearcy.org>
2019-03-09 14:39:04 +1100

    Displays tile values
    Holding shift now shows menu

--- MapCreator.py
+++ MapCreator.py
@@ -37,15 +37,6 @@ while True:
             mapcreatorfunctions.end(grid)
         if event.type == pygame.KEYDOWN:
             if pygame.key.get_mods() & pygame.KMOD_SHIFT:
-                if event.key == pygame.K_UP:
-                    area[1] += .2
-                if event.key == pygame.K_RIGHT:
-                    area[0] -= .2
-                if event.key == pygame.K_DOWN:
-                    area[1] -= .2
-                if event.key == pygame.K_LEFT:
-                    area[0] += .2
-            else:
                 if event.key == pygame.K_RIGHT:
                     page += 1
                 if event.key == pygame.K_LEFT and page > 0:
@@ -56,6 +47,15 @@ while True:
                 if event.key == pygame.K_DOWN and selectedNum < len(tiles)-1:
                     selectedNum += 1
                     selected = tiles[selectedNum]
+            else:
+                if event.key == pygame.K_UP:
+                    area[1] += .2
+                if event.key == pygame.K_RIGHT:
+                    area[0] -= .2
+                if event.key == pygame.K_DOWN:
+                    area[1] -= .2
+                if event.key == pygame.K_LEFT:
+                    area[0] += .2
 
         if event.type == pygame.MOUSEBUTTONDOWN:
             location = [(event.pos[0]-area[0]*100)//100, (event.pos[1]-area[1]*100)//100]
@@ -89,10 +89,13 @@ while True:
         spot += 50
 
     menu.blit(selected.img, [300-110, 10])
+    menu.blit(font.render(str(selected.dfce), True, black), [300-110, 110])
+    menu.blit(font.render(str(selected.res), True, black), [300-110, 125])
+    menu.blit(font.render(str(selected.mov), True, black), [300-110, 140])
     pygame.draw.rect(menu, black, [300-110, 10, 100, 100], 1)
 
     # Print menu
-    if not pygame.key.get_mods() & pygame.KMOD_SHIFT:
+    if pygame.key.get_mods() & pygame.KMOD_SHIFT:
         screen.blit(menu, [0, 700-300])
 
     pygame.display.flip()
